825a7a9bb0e45fd89081b164a24ba577da1546d3
makamys <makamys@outlook.com>
2021-07-27 06:58:37 +0200

    Refactor tooltip drawing

--- src/main/java/makamys/updatechecklib/GuiButtonGeneric.java
+++ src/main/java/makamys/updatechecklib/GuiButtonGeneric.java
@@ -1,8 +1,12 @@
 package makamys.updatechecklib;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.lwjgl.opengl.GL11;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.renderer.OpenGlHelper;
@@ -40,45 +44,13 @@ public class GuiButtonGeneric extends GuiButton {
     public void drawButton(Minecraft p_146112_1_, int mouseX, int mouseY) {
     	super.drawButton(p_146112_1_, mouseX, mouseY);
     	
-    	GuiScreen current = Minecraft.getMinecraft().currentScreen;
-    	
     	boolean inWindow = this.enabled && this.visible && mouseX >= this.xPosition && mouseY >= this.yPosition && mouseX < this.xPosition + this.width && mouseY < this.yPosition + this.height;
     	
-    	if(current != null && inWindow) {
-    		int startY = 40;
-    		int startX = 30;
-    		int width = 150;
-    		int textOff = 3;
-    		
-    		startX = mouseX - 70;
-    		startY = mouseY - 30;
-    		
-	        GL11.glDisable(GL11.GL_TEXTURE_2D);
-	        GL11.glEnable(GL11.GL_BLEND);
-	        GL11.glDisable(GL11.GL_ALPHA_TEST);
-	        OpenGlHelper.glBlendFunc(770, 771, 1, 0);
-	        GL11.glShadeModel(GL11.GL_SMOOTH);
-	        Tessellator tessellator = Tessellator.instance;
-	        tessellator.startDrawingQuads();
-	        tessellator.setColorRGBA_F(0, 0, 0, 0.7f);
-	        int p_73733_3_ = startX;
-	        int p_73733_1_ = startX + width;
-	        int p_73733_4_ = startY;
-	        int p_73733_2_ = startY+30+5;
-	        tessellator.addVertex((double)p_73733_3_, (double)p_73733_2_, (double)this.zLevel);
-	        tessellator.addVertex((double)p_73733_1_, (double)p_73733_2_, (double)this.zLevel);
-	        //tessellator.setColorRGBA_F(f5, f6, f7, f4);
-	        tessellator.addVertex((double)p_73733_1_, (double)p_73733_4_, (double)this.zLevel);
-	        tessellator.addVertex((double)p_73733_3_, (double)p_73733_4_, (double)this.zLevel);
-	        tessellator.draw();
-	        GL11.glShadeModel(GL11.GL_FLAT);
-	        GL11.glDisable(GL11.GL_BLEND);
-	        GL11.glEnable(GL11.GL_ALPHA_TEST);
-	        GL11.glEnable(GL11.GL_TEXTURE_2D);
-	        
-	        current.drawString(Minecraft.getMinecraft().fontRenderer, EnumChatFormatting.GREEN + "4" + EnumChatFormatting.RESET + " mod updates found.", startX + textOff, startY + textOff, 0xFFFFFF);
-	        current.drawString(Minecraft.getMinecraft().fontRenderer, "Click to open list in browser.", startX + textOff, startY + textOff + 10, 0xFFFFFF);
-	        current.drawString(Minecraft.getMinecraft().fontRenderer, "(Shift click to copy URL.)", startX + textOff, startY + textOff + 20, 0xBBBBBB);
+    	if(inWindow) {
+    		Util.drawSimpleTooltip(mouseX, mouseY, (int)this.zLevel, Arrays.asList(
+	        		EnumChatFormatting.GREEN + "4" + EnumChatFormatting.RESET + " mod updates found.",
+	        		"Click to open list in browser.",
+	        		EnumChatFormatting.GRAY + "(Shift click to copy URL.)"));
     	}
     }
     
--- src/main/java/makamys/updatechecklib/Util.java
+++ src/main/java/makamys/updatechecklib/Util.java
@@ -1,6 +1,16 @@
 package makamys.updatechecklib;
 
 import java.net.URI;
+import java.util.Arrays;
+import java.util.List;
+
+import org.lwjgl.opengl.GL11;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
 
 public class Util {
 	
@@ -12,6 +22,49 @@ public class Util {
 		} catch (Throwable throwable) {
 			UpdateCheckLib.LOGGER.error("Couldn\'t open link", throwable);
 		}
+	}
+	
+	/** A simple alternative to Minecraft's tooltip drawing function in case it can't be used. */
+	public static void drawSimpleTooltip(int mouseX, int mouseY, int z, List<String> lines) {
+		GuiScreen current = Minecraft.getMinecraft().currentScreen;
+		
+		FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
+		
+		int margin = 2;
+        
+        int width = lines.stream().mapToInt(s -> fr.getStringWidth(s)).max().getAsInt();
+        int height = lines.size() * 10;
+        
+		int startX = mouseX - width / 2;
+		int startY = mouseY - height - 5;
+        
+        GL11.glDisable(GL11.GL_TEXTURE_2D);
+        GL11.glEnable(GL11.GL_BLEND);
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
+        OpenGlHelper.glBlendFunc(770, 771, 1, 0);
+        GL11.glShadeModel(GL11.GL_SMOOTH);
+        Tessellator tessellator = Tessellator.instance;
+        tessellator.startDrawingQuads();
+        tessellator.setColorRGBA_F(0, 0, 0, 0.7f);
+        int p_73733_3_ = startX - margin;
+        int p_73733_1_ = startX + width + margin * 2;
+        int p_73733_4_ = startY - margin;
+        int p_73733_2_ = startY + height + margin * 2;
+        tessellator.addVertex((double)p_73733_3_, (double)p_73733_2_, (double)z);
+        tessellator.addVertex((double)p_73733_1_, (double)p_73733_2_, (double)z);
+        tessellator.addVertex((double)p_73733_1_, (double)p_73733_4_, (double)z);
+        tessellator.addVertex((double)p_73733_3_, (double)p_73733_4_, (double)z);
+        tessellator.draw();
+        GL11.glShadeModel(GL11.GL_FLAT);
+        GL11.glDisable(GL11.GL_BLEND);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
+        GL11.glEnable(GL11.GL_TEXTURE_2D);
+        
+        int y = 0;
+        for(String s : lines) {
+        	current.drawString(Minecraft.getMinecraft().fontRenderer, s, startX + margin, startY + margin + y, 0xFFFFFF);
+        	y += 10;
+        }
 	}
 	
 }
