c6e5181fbcb80461029681e38be27a45beb868c2
makamys <makamys@outlook.com>
2021-07-27 03:54:19 +0200

    Add test class

---
+++ src/main/java/makamys/updatechecklib/test/UCLTest.java
@@ -0,0 +1,26 @@
+package makamys.updatechecklib.test;
+
+import cpw.mods.fml.common.Loader;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import makamys.updatechecklib.UpdateCheckLib;
+
+@Mod(modid = UCLTest.MODID, version = "0.0")
+public class UCLTest {
+	
+	public static final String MODID = "UpdateCheckLibTest"; 
+	
+	@EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+    	if(Loader.isModLoaded("UpdateCheckLib")) {
+    		initUpdateCheck();
+    	}
+    }
+    
+    @cpw.mods.fml.common.Optional.Method(modid = "UpdateCheckLib")
+    private void initUpdateCheck() {
+    	UpdateCheckLib.submitModTask(MODID, "https://raw.githubusercontent.com/makamys/MAtmos/master/updatejson/update-matmos.json");
+    }
+    
+}
